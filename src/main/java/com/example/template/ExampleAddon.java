3916f41a694a90fc497dfffaec1d0d54943cfdda
Lennart Lösche <loesche.lennart@gmail.com>
2022-12-28 23:54:55 +0100

    update template to labymod addon template from mod template

---
+++ src/main/java/com/example/template/ExampleAddon.java
@@ -0,0 +1,23 @@
+package main.java.com.example.template;
+
+import java.util.List;
+import net.labymod.api.LabyModAddon;
+import net.labymod.settings.elements.SettingsElement;
+
+public class ExampleAddon extends LabyModAddon {
+
+  @Override
+  public void onEnable() {
+
+  }
+
+  @Override
+  public void loadConfig() {
+
+  }
+
+  @Override
+  protected void fillSettings(List<SettingsElement> list) {
+
+  }
+}
